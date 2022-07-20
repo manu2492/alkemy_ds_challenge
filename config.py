8f696c794deae01107d4087b15ff3c2f35751387
Emanuel Schemberger <emanueldev24@gmail.com>
2022-07-20 11:01:55 -0300

    day 5:finish

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 from decouple import config
+
+"""
+    This file contains the configuration for the application.
+    decouple is a tool that allows us to store sensitive data in a separate file.
+    documentation: https://pypi.org/project/python-decouple/
+"""
+
 USER = config("USUARIO")
 PASSWORD = config('PASS')
 DB_NAME = config('DB_NAME')
